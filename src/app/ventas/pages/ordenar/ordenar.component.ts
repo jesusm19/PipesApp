7ae74dade37e33dcc62c94885646b0aa711ff1e6
Jesus Garcia <jesus_heavy19@hotmail.com>
2023-04-04 17:45:22 -0600

    pipes personalizados, ordenar y mayusculas

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html'
 })
 export class OrdenarComponent {
+
+  isMayuscula: boolean = true;
+  heroes: Heroe [] = [
+    {nombre: 'superman', vuela: true, color: Color.azul},
+    {nombre: 'batman', vuela: false, color: Color.negro},
+    {nombre: 'ironman', vuela: true, color: Color.rojo},
+    {nombre: 'hulk', vuela: false, color: Color.verde}
+  ];
+
+  ordenarPor: string = '';
+
+  convertirMayusculaMinuscula(){
+    if(this.isMayuscula){
+      this.isMayuscula = false;
+      return;
+    }
+    this.isMayuscula = true;
+  }
+
+  ordenarPorTipo(tipoOrden: string){
+    this.ordenarPor = tipoOrden;
+
+  }
 
 }
